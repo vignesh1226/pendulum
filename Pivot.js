50b7eed924286e02d7c1a1ea0c69931514200a07
vignesh1226 <61319256+vignesh1226@users.noreply.github.com>
2020-03-18 07:25:54 +0530

    Update Pivot.js

--- Pivot.js
+++ Pivot.js
@@ -1,17 +1,18 @@
 class Pivot {
+    //constructing the body
     constructor(x,y,width,height) {
-
+      //giving it options
       var options = {
           isStatic: true
       }
-      
+      //shape of the body
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
     
     }
-
+    //displaying the body
     display(){
     
       var pos =this.body.position;
