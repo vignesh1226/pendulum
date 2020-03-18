44be1ae9e8f1c62682d650282ebe87cb1717c250
vignesh1226 <61319256+vignesh1226@users.noreply.github.com>
2020-03-18 07:23:53 +0530

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -1,18 +1,19 @@
 class Bob {
+    //constructing a ball
     constructor(x,y,radius) {
-
+      //giving the options to it 
       var options = {
           density : 1.0,
           restitution : 1.0,
           frictionAir : 0.04
       }
-
+      //shape of the body
       this.body = Bodies.circle(x,y,radius,options);
       this.radius = radius;
       World.add(world, this.body);
     
     }
-    
+    //displaying the body
     display(){
 
       var pos =this.body.position;
